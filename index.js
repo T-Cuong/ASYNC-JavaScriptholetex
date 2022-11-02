51b107004c9c654ef4fc507131ba14e8bf7c81c5
T-Cuong <trongcuong17pfiev@gmail.com>
2022-11-02 15:42:48 +0700

    ASYNC JavaScriptholetex

---
+++ index.js
@@ -0,0 +1,24 @@
+//https://www.youtube.com/watch?v=XYVJKnZc0SY&list=PLqQ6Lvascx2tbLuhCg3E1nC1qcHrckpue&index=2
+// dong bo la code chay tu tren xuong duoi
+/* bat dong bo la code chay ko theo thu tu tu tren xuong, bat dong bo giup ta lam nhieu 
+ viec cung 1 luc, co 3 cach xu ly bat dong bo la: callback, promise va async await 
+ doi voi bat dong bo trong js co 2 loai chinh: 
+ 1. Browser API/Web API cung cap cac function/event de xu ly bat dong bo:
+ -setTimeout(), setInterval()...
+ -click, scroll,...
+ 2. Promises 
+ - giup ta thuc hien cac tac vu bat dong bo
+ 
+ Tim hieu ve callback?
+ - Trong js function hoat dong nhu 1 obj cho nen ta co the truyen 1 function vao fuction khac nhu 1 tham so ta goi day la callback 5:10
+ - callbackhell 7:17
+ Tim hieu ve promise?
+ - De xu ly callback hell nguoi ta  dung promise, promise la 1 obj de dai dien cho viec thuc hien 1 hanh dong bat dong bo thanh cong hay that bai
+ - Promise co 2 tinh huong (thuc hien loi hua , that hua)
+ - Promise  co 3 trang thai: 
+   1. pending ( dang cho thuc thi 1 hanh dong bat dong bo va chua biet duoc thanh cong hay that bai) gia su: khi ta co 1 promise va trong promise ta 
+   goi den api luc nay khi chua co thong tin request nay thanh cong hay ko thi trang thai cua promise la pending va khi co ket qua tra ve tu api neu thanh cong
+   thi no co trang thai la resolve, nguoc lai neu du lieu tra ve la fail thi trang thai tra ve la reject 11:12
+ Tim hieu ve async await
+ - Async await cung la 1 promise va no giup cho chung ta viet 1 doan code bat dong bo trong nhu dong bo chung ta co the de dang doc va hieu 19:37
+ */ 
